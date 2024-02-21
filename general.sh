73a4241bc32fbd0818a5459f0f4e0cff279d83d0
xale <xalelu@users.noreply.github.com>
2024-02-21 23:52:57 +0900

    Update general.sh 10

--- general.sh
+++ general.sh
@@ -64,8 +64,61 @@ fi
 filepath="./client.c"
 if [ ! -f "$filepath" ];then
     touch $filepath
-    echo "XXXX" >> $filepath
-    echo "YYYY" >> $filepath
+    echo "#include <stdio.h>" >> $filepath
+    echo "#include <stdlib.h>" >> $filepath
+    echo "#include <unistd.h>" >> $filepath
+    echo "#include <string.h>" >> $filepath
+    echo "#include <sys/types.h>" >> $filepath
+    echo "#include <sys/socket.h>" >> $filepath
+    echo "#include <netinet/in.h>" >> $filepath
+    echo "#include <netdb.h>" >> $filepath
+    echo "" >> $filepath
+    echo "void error(const char *msg)" >> $filepath
+    echo "{" >> $filepath
+    echo "    perror(msg);" >> $filepath
+    echo "    exit(0);" >> $filepath
+    echo "}" >> $filepath
+    echo "" >> $filepath
+    echo "int main(int argc, char *argv[])" >> $filepath
+    echo "{" >> $filepath
+    echo "    int sockfd, portno, n;" >> $filepath
+    echo "    struct sockaddr_in serv_addr;" >> $filepath
+    echo "    struct hostent *server;" >> $filepath
+
+    echo "    char buffer[256];" >> $filepath
+    echo "    if (argc < 3) {" >> $filepath
+    echo "        fprintf(stderr,\"usage %s hostname port\n\", argv[0]);" >> $filepath
+    echo "        exit(0);" >> $filepath
+    echo "    }" >> $filepath
+    echo "    portno = atoi(argv[2]);" >> $filepath
+    echo "    sockfd = socket(AF_INET, SOCK_STREAM, 0);" >> $filepath
+    echo "    if (sockfd < 0) {" >> $filepath
+    echo "        error("ERROR opening socket");" >> $filepath
+    echo "    server = gethostbyname(argv[1]);" >> $filepath
+    echo "    if (server == NULL) {" >> $filepath
+    echo "        fprintf(stderr,\"ERROR, no such host\n\");" >> $filepath
+    echo "        exit(0);" >> $filepath
+    echo "    }" >> $filepath
+    echo "    bzero((char *) &serv_addr, sizeof(serv_addr));" >> $filepath
+    echo "    serv_addr.sin_family = AF_INET;" >> $filepath
+    echo "    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);" >> $filepath
+    echo "    serv_addr.sin_port = htons(portno);" >> $filepath
+    echo "    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){" >> $filepath 
+    echo "        error(\"ERROR connecting\");" >> $filepath
+    echo "    printf(\"Please enter the message: \");" >> $filepath
+    echo "    bzero(buffer,256);" >> $filepath
+    echo "    fgets(buffer,255,stdin);" >> $filepath
+    echo "    n = write(sockfd,buffer,strlen(buffer));" >> $filepath
+    echo "    if (n < 0) " >> $filepath
+    echo "        error(\"ERROR writing to socket\");" >> $filepath
+    echo "    bzero(buffer,256);" >> $filepath
+    echo "    n = read(sockfd,buffer,255);" >> $filepath
+    echo "    if (n < 0){ " >> $filepath
+    echo "        error(\"ERROR reading from socket\");" >> $filepath
+    echo "    printf("%s\n",buffer);" >> $filepath
+    echo "    close(sockfd);" >> $filepath
+    echo "    return 0;" >> $filepath
+    echo "}" >> $filepath
     chmod 777 "$filepath"
 else
     echo "file client.c has be exists."
