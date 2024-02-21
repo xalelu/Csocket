26411fb658f7e8dd4a58128f6138c49054592e2c
xale <xalelu@users.noreply.github.com>
2024-02-21 23:23:52 +0900

    Update general.sh 6

--- general.sh
+++ general.sh
@@ -5,6 +5,7 @@ if [ ! -f "$filepath" ];then
     touch $filepath
     echo "AAAA" >> $filepath
     echo "BBBB" >> $filepath
+    chmod 777 "$filepath"
 else
     echo "file server.c has be exists.."
 fi
@@ -14,6 +15,7 @@ if [ ! -f "$filepath" ];then
     touch $filepath
     echo "XXXX" >> $filepath
     echo "YYYY" >> $filepath
+    chmod 777 "$filepath"
 else
     echo "file client.c has be exists."
 fi
