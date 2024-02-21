27a8ea9b8f3e79fab755ac2e7e7455b7bfa22e5b
xale <xalelu@users.noreply.github.com>
2024-02-21 23:07:29 +0900

    Update general.sh

--- general.sh
+++ general.sh
@@ -3,7 +3,8 @@
 filepath="./server.c"
 if [ -f "$filepath" ];then
     touch $filepath
-    echo "" > $filepath
+    echo "AAAA" > $filepath
+    echo "BBBB" > $filepath
 else
     echo "file server.c has be exists.."
 fi
@@ -11,6 +12,7 @@ fi
 filepath="./client.c"
 if [ -f "$filepath" ];then
     touch $filepath
-    echo "" > $filepath
+    echo "XXXX" > $filepath
+    echo "YYYY" > $filepath
 else
     echo "file client.c has be exists."
