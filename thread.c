ddf31b0dd2b432644eb8e6cc45ca82f56c968b2d
xale <xalelu@users.noreply.github.com>
2024-02-26 00:01:47 +0900

    Create thread.c

---
+++ thread.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void *
+threadFunc(void *arg)
+{
+    char *s = (char *) arg;
+
+    printf("%s", s);
+
+    return  (void *) strlen(s);
+}
+
+int
+main (int argc, char **argv)
+{
+    pthread_t t1;
+    void *res;
+    int s;
+
+    s = pthread_create(&t1, NULL, threadFunc, "Hello world\n");
+    if (s != 0) {
+        fprintf(stderr, "error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message from main()\n");
+    s = pthread_join(t1, &res);
+    if (s != 0) {
+        fprintf(stderr, "error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%hread returned $ld\nk", (long) res);
+    
+    exit(EXIT_SUCCESS);
+}
